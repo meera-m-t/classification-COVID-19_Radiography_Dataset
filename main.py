9794a71d041bffd5b32031281e77c160f8abf575
meera-m-t <sameerah.talafha@siu.edu>
2022-02-25 20:49:18 -0600

    edit main.py

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ import tensorflow as tf
 from SVDNet import SVDNet
 from SVRNet import SVRNet
 import skimage.io as io
-import glob
+from sklearn.metrics import confusion_matrix
+import os
+from sklearn.model_selection import train_test_split
 import numpy as np
 import matplotlib.pyplot as plt
-from tensorflow.keras.callbacks import EarlyStopping 
+from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow import keras
 from tensorflow.keras.preprocessing import image_dataset_from_directory
 from tensorflow.data.experimental import AUTOTUNE
+import seaborn as sns
+sns.set()
 
 def train_val_split(DATA_DIR,IMG_SIZE):
 
@@ -48,41 +52,54 @@ def  call_test_data(DATA_DIR,IMG_SIZE):
                     labels="inferred",
                     label_mode="categorical",
                     color_mode="grayscale",   
-                    batch_size=32,                 
+                    batch_size=32,                                   
                     image_size=(IMG_SIZE, IMG_SIZE),
-                    validation_split=0.2,
-                    subset="validation",
+                    validation_split=None,
+                    subset=None,
                     seed=1
                 )
-    return test_data          
+    return test_data.cache().prefetch(buffer_size=AUTOTUNE)         
 
-if __name__ == '__main__':   
+if __name__ == '__main__':  
     path_train='output/train' 
-    path_test='output/test'  
+    path_test='output/test' 
 
     length = 299  # Length of each Image
     width = 299  # Width of each Image
     # model_name = 'VGG16_v2'  # DenseNet Models
-    model_name = 'SVRNet'   # DenseNet Models   
+    model_name = 'SVRNet'   # DenseNet Models  
     # model_name = 'SVDNet'   # DenseNet Models
-    model_width = 3 # Width of the Initial Layer, subsequent layers start from here
+    model_width = 16 # Width of the Initial Layer, subsequent layers start from here
     num_channel = 1  # Number of Input Channels in the Model
     problem_type = 'Classification' # Classification or Regression
-    output_nums = 3  # Number of Class for Classification Problems, always '1' for Regression Problems
-  
+    output_nums = 2  # Number of Class for Classification Problems, always '1' for Regression Problems
+ 
     train_data, val_data = train_val_split(path_train, length)
-    test_data = call_test_data(path_test, length) 
+    test_data = call_test_data(path_test, length)
+
     BATCH_SIZE = 32
-    EPOCHS = 100
     
-    # model = VGG(length, width, num_channel, model_width, problem_type=problem_type, output_nums=output_nums, dropout_rate=0.5).VGG16_v2()
-    # model = SVDNet(length, width, num_channel, model_width, problem_type=problem_type, output_nums=output_nums, pooling='max', dropout_rate=0.5).SVDNet()   
-    model = SVRNet(length, width, num_channel, model_width, problem_type=problem_type, output_nums=output_nums, pooling='max', dropout_rate=0.5).SVRNet()  
+
+
+    if os.path.isdir("SVRNet_model"): 
+        # model = keras.models.load_model('vgg_model')
+        # model = tf.keras.models.load_model('SVDNet_model')
+        EPOCHS = 1
+        model = tf.keras.models.load_model('SVRNet_model')
+    else:
+        EPOCHS = 100  
+        #   model = VGG(length, width, num_channel, model_width, problem_type=problem_type, output_nums=output_nums, dropout_rate=0.5).VGG16_v2()
+    #     # # model = SVDNet(length, width, num_channel, model_width, problem_type=problem_type, output_nums=output_nums, pooling='max', dropout_rate=0.5).SVDNet()  
+        model = SVRNet(length, width, num_channel, model_width, problem_type=problem_type, output_nums=output_nums, pooling='max', dropout_rate=0.5).SVRNet()  
 
     model.summary()
 
+    dot_img_file = 'model_SVRNet.png'
+    tf.keras.utils.plot_model(model, to_file=dot_img_file, show_shapes=True)
+
+
     model.compile(
-        optimizer=tf.keras.optimizers.Adam(learning_rate=0.0003),
+        optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001),
         loss=tf.keras.losses.CategoricalCrossentropy(),
         metrics=[tf.keras.metrics.CategoricalAccuracy()])
 
@@ -92,13 +109,27 @@ if __name__ == '__main__':
         validation_data=val_data,  
         epochs=EPOCHS,
         callbacks=callback,
-        batch_size=BATCH_SIZE,   
+        batch_size=BATCH_SIZE,  
         shuffle=True
         )
     print(history.history.keys())
-    model.save("my_model")
-    loss, acc = model.evaluate(test_data, verbose = 0)
+
+    if not os.path.isdir("SVRNet_model"):
+        model.save("SVRNet_model")
+
+    loss, acc = model.evaluate(test_data, verbose = 0, batch_size=32)
     print("Loss {}, Accuracy {}".format(loss, acc))
+
+
+
+    predictions = np.array([])
+    labels =  np.array([])
+    for x, y in test_data:
+        predictions = np.concatenate([predictions, np.argmax(model.predict(x), axis=1)])
+        labels = np.concatenate([labels, np.argmax(y.numpy(), axis=-1)])
+
+    cf_matrix = tf.math.confusion_matrix(labels=labels, predictions=predictions).numpy()
+
 
     # summarize history for accuracy
     plot1 = plt.figure(1)
@@ -108,9 +139,9 @@ if __name__ == '__main__':
     plt.ylabel('accuracy')
     plt.xlabel('epoch')
     plt.legend(['train', 'val'], loc='upper left')
-    plt.savefig('model_accuracy.png')  
-    # # summarize history for loss
+    plt.savefig('model_SVRNet_accuracy.png') 
 
+    # # summarize history for loss
     plot2 = plt.figure(2)
     plt.plot(history.history['loss'])
     plt.plot(history.history['val_loss'])
@@ -118,4 +149,19 @@ if __name__ == '__main__':
     plt.ylabel('loss')
     plt.xlabel('epoch')
     plt.legend(['train', 'val'], loc='upper left')
-    plt.savefig('model_lost.png')
+    plt.savefig('model_SVRNet_lost.png')
+
+    #confusion_matrix
+    ax = sns.heatmap(cf_matrix/np.sum(cf_matrix), annot=True,
+            fmt='.2%', cmap='Blues')
+
+    ax.set_title('Seaborn Confusion Matrix with labels\n\n')
+    ax.set_xlabel('\nPredicted Values')
+    ax.set_ylabel('Actual Values ')
+
+    ## Ticket labels - List must be in alphabetical order
+    ax.xaxis.set_ticklabels(['False','True'])
+    ax.yaxis.set_ticklabels(['False','True'])
+    fig = ax.get_figure()
+    fig.savefig('model_SVRNet_confusion_matrix.png')
+
