0a0a1e58263669c7018f3044784eaf0d43b38f5d
meera-m-t <sameerah.talafha@siu.edu>
2022-03-03 16:29:09 -0600

    following the suggestions

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ def  call_test_data(DATA_DIR,IMG_SIZE):
     return test_data.cache().prefetch(buffer_size=AUTOTUNE)         
 
 def main():
-    parser = argparse.ArgumentParser(description='add, modify and delete upstream nodes')
+    parser = argparse.ArgumentParser(description='Train different models using the COVID-19 datasets')
     parser.add_argument(
        '-p_train','--path_train', required=False, default='output/train', type=str, help='path train')
     parser.add_argument(
@@ -85,8 +85,6 @@ def main():
           '-b', '--batch_size', required=False, default=32, type=int, help='batch size')
   
     args = parser.parse_args()
-
-
     path_train = args.path_train
     path_test = args.path_test
     length = args.length  
@@ -124,14 +122,11 @@ def main():
         else:
             EPOCHS = 100
             model = SVDNet(length, width, num_channel, model_width, problem_type=problem_type, output_nums=output_nums, pooling='max', dropout_rate=0.5).SVDNet()
-                    
-
+    
     model.summary()
-
     dot_img_file = f'{model_name}.png'
+
     tf.keras.utils.plot_model(model, to_file=dot_img_file, show_shapes=True)
-
-
     model.compile(
         optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001),
         loss=tf.keras.losses.CategoricalCrossentropy(),
@@ -145,16 +140,13 @@ def main():
         callbacks=callback,
         batch_size=BATCH_SIZE,  
         shuffle=True
-        )
-    print(history.history.keys())
+        )   
 
     if not os.path.isdir(f'{model_name}_model'):
         model.save(f'{model_name}_model')
 
     loss, acc = model.evaluate(test_data, verbose = 0)
     print("Loss {}, Accuracy {}".format(loss, acc))
-
-
 
     predictions = np.array([])
     labels =  np.array([])
@@ -163,8 +155,6 @@ def main():
         labels = np.concatenate([labels, np.argmax(y.numpy(), axis=-1)])
 
     cf_matrix = tf.math.confusion_matrix(labels=labels, predictions=predictions).numpy()
-
-
     # summarize history for accuracy
     plot1 = plt.figure(1)
     plt.plot(history.history['categorical_accuracy'])
@@ -173,7 +163,7 @@ def main():
     plt.ylabel('accuracy')
     plt.xlabel('epoch')
     plt.legend(['train', 'val'], loc='upper left')
-    plt.savefig('SVDNet_model_accuracy.png') 
+    plt.savefig(f'{model_name}_model_accuracy.png') 
 
     # # summarize history for loss
     plot2 = plt.figure(2)
@@ -183,13 +173,11 @@ def main():
     plt.ylabel('loss')
     plt.xlabel('epoch')
     plt.legend(['train', 'val'], loc='upper left')
-    plt.savefig(f'{model_name}_model_lost.png')
+    plt.savefig(f'{model_name}_model_loss.png')
 
     #confusion_matrix
     plot3 = plt.figure(3)
-    ax = sns.heatmap(cf_matrix/np.sum(cf_matrix), annot=True,
-            fmt='.2%', cmap='Blues')
-
+    ax = sns.heatmap(cf_matrix/np.sum(cf_matrix), annot=True,fmt='.2%', cmap='Blues')
     ax.set_title('Confusion Matrix with labels\n\n')
     ax.set_xlabel('\nPredicted Values')
     ax.set_ylabel('Actual Values ')
@@ -200,9 +188,5 @@ def main():
     fig = ax.get_figure()
     fig.savefig(f'{model_name}_model_confusion_matrix.png')
 
-
 if __name__ == "__main__":
     main()
-
-
-
--- preprocessing_data.py
+++ preprocessing_data.py
@@ -4,14 +4,34 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 import splitfolders
-for name in ['COVID',  'Normal']:
-    for file in glob.glob(f"COVID-19_Radiography_Dataset/{name}/*.png"):
-        name_file = os.path.basename(file)        
-        image = cv2.imread(file)
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
-        equalized = clahe.apply(gray)    
-        cv2.imwrite(f"COVID-19_Radiography_Dataset/preprocessed_data/{name}/{name_file}", equalized)
+import os
 
+def preprocess(path_dataset, path_workspace):
+    names = [item for item in os.listdir(path_dataset) if os.path.isdir(os.path.join(path_dataset, item))]
+    for name in names:
+        for file in glob.glob(f"{path}/{name}/*.png"):
+            name_file = os.path.basename(file)
+            image = cv2.imread(file)
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+            equalized = clahe.apply(gray)
+            os.makedirs('{path_workspace}/preprocessed_data')
+            cv2.imwrite(f"{path_workspace}/preprocessed_data/{name}/path_workspace {name_file", equalized)
 
-splitfolders.ratio('COVID-19_Radiography_Dataset/preprocessed_data', output="output", seed=1337, ratio=(.8, 0.2))
+def split_data(path):
+    splitfolders.ratio(f'{path/preprocessed_data}', output="path/output", seed=1337, ratio=(.8, 0.2))
+
+def main():
+    parser = argparse.ArgumentParser(description='add, modify and delete upstream nodes')
+    parser.add_argument(
+       '-p','--path_dataset', required=False, default='COVID-19_Radiography_Dataset', type=str, help='path of dataset')
+    parser.add_argument(
+       '-p_ws','--path_workspace', required=False, default='classification-COVID-19_Radiography_Dataset', type=str, help='path of dataset')    
+    args = parser.parse_args()
+    path_dataset = args.path_dataset
+    path_workspace = args.path_workspace
+    preprocess(path_dataset ,path_workspace)
+    split_data(path_workspace)
+
+if __name__ == "__main__":
+    main()
